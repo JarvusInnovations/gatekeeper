419196a659554c4c9bdc9433ef90b18514f54b31
Chris Alfano <chris@jarv.us>
2014-02-11 19:14:31 -0500

    implemented ban enforcement

--- php-classes/ApiRequestHandler.class.php
+++ php-classes/ApiRequestHandler.class.php
@@ -2,17 +2,29 @@
 
 class ApiRequestHandler extends RequestHandler
 {
-	static public $sourceInterface = null; // string=hostname or IP, null=http hostname, false=let cURL pick
-	
+    static public $sourceInterface = null; // string=hostname or IP, null=http hostname, false=let cURL pick
+
 	static public function handleRequest() {
-		
+
 		// check required parameters
 		if (!$endpointHandle = static::shiftPath()) {
 			return static::throwInvalidRequestError('Endpoint handle required');
 		}
-		
+
 		if (!($endpointVersion = static::shiftPath()) || !preg_match('/^v.+$/', $endpointVersion)) {
 			return static::throwInvalidRequestError('Endpoint version required');
+		}
+
+
+		// get active bans
+		//	- cache entire list of currently active bans in an array, maybe this will prove less efficient when the list grows
+		$activeBans = Ban::getActiveBansTable();
+
+
+		// check if IP is banned
+		if (in_array($_SERVER['REMOTE_ADDR'], $activeBans['ips'])) {
+			header('HTTP/1.1 403 Forbidden');
+			JSON::error('Your IP address is currently banned from using this service');
 		}
 
 
@@ -20,14 +32,14 @@ class ApiRequestHandler extends RequestHandler
 		if (!$Endpoint = Endpoint::getByWhere(array('Handle' => $endpointHandle, 'Version' => substr($endpointVersion, 1)))) {
 			return static::throwNotFoundError('Requested endpoint not found');
 		}
-		
+
 		$endpointVersion = substr($endpointVersion, 1);
-		
+
 		if ($endpointVersion != $Endpoint->Version) {
 			return static::throwNotFoundError('Requested endpoint version not available');
 		}
-		
-		
+
+
 		// verify key if required
 		if ($Endpoint->KeyRequired) {
 			if (!empty($_SERVER['HTTP_AUTHORIZATION']) && preg_match('/^GateKeeper-Key\s+(\w+)$/i', $_SERVER['HTTP_AUTHORIZATION'], $keyMatches)) {
@@ -35,59 +47,66 @@ class ApiRequestHandler extends RequestHandler
 			} elseif (!empty($_REQUEST['gatekeeperKey'])) {
 				$keyString = $_REQUEST['gatekeeperKey'];
 			}
-			
+
 			if (!$keyString) {
 				return static::throwKeyError($Endpoint, 'gatekeeper key required for this endpoint');
 			} elseif(!$Key = Key::getByKey($keyString)) {
 				return static::throwKeyError($Endpoint, 'gatekeeper key invalid');
 			} elseif(!$Key->AllEndpoints) {
+				// TODO: check explicit endpoints list
 				return static::throwKeyError($Endpoint, 'gatekeeper key valid but does not permit this endpoint');
 			}
-		}
-		
-		
-		// build identifier string for current user
-		$userKey = $Key ? "keys/$Key-ID" : "ips/$_SERVER[REMOTE_ADDR]";
-		
-		
-		// drip into endpoint+user bucket first so that abusive users can't polute the global bucket
-        if ($Endpoint->UserRatePeriod && $Endpoint->UserRateCount) {
-    		$retryAfter = HitBuckets::drip("endpoints/$Endpoint->ID/$userKey", function() use ($Endpoint) {
-    			return array('seconds' => $Endpoint->UserRatePeriod, 'count' => $Endpoint->UserRateCount);
-    		});
-    		
-    		if ($retryAfter) {
-    			return static::throwRateError($retryAfter, 'Your rate limit for this endpoint has been exceeded');
-    		}
-        }
 
-		
+			// check if key is banned
+			if (in_array($Key->ID, $activeBans['keys'])) {
+				header('HTTP/1.1 403 Forbidden');
+				JSON::error('Your API key is currently banned from using this service');
+			}
+		}
+
+
+		// build identifier string for current user
+		$userKey = $Key ? "keys/$Key->ID" : "ips/$_SERVER[REMOTE_ADDR]";
+
+
+		// drip into endpoint+user bucket first so that abusive users can't polute the global bucket
+		if ($Endpoint->UserRatePeriod && $Endpoint->UserRateCount) {
+			$retryAfter = HitBuckets::drip("endpoints/$Endpoint->ID/$userKey", function() use ($Endpoint) {
+				return array('seconds' => $Endpoint->UserRatePeriod, 'count' => $Endpoint->UserRateCount);
+			});
+
+			if ($retryAfter) {
+				return static::throwRateError($retryAfter, 'Your rate limit for this endpoint has been exceeded');
+			}
+		}
+
+
 		// TODO: implement a per-user throttle that applies across all endpoints? Might not be useful...
-		
-		
+
+
 		// drip into endpoint bucket
-        if ($Endpoint->GlobalRatePeriod && $Endpoint->GlobalRateCount) {
-    		$retryAfter = HitBuckets::drip("endpoints/$Endpoint->ID", function() use ($Endpoint) {
-    			return array('seconds' => $Endpoint->GlobalRatePeriod, 'count' => $Endpoint->GlobalRateCount);
-    		});
-    		
-    		if ($retryAfter) {
-    			return static::throwRateError($retryAfter, 'The global rate limit for this endpoint has been exceeded');
-    		}
-        }
+		if ($Endpoint->GlobalRatePeriod && $Endpoint->GlobalRateCount) {
+			$retryAfter = HitBuckets::drip("endpoints/$Endpoint->ID", function() use ($Endpoint) {
+				return array('seconds' => $Endpoint->GlobalRatePeriod, 'count' => $Endpoint->GlobalRateCount);
+			});
+
+			if ($retryAfter) {
+				return static::throwRateError($retryAfter, 'The global rate limit for this endpoint has been exceeded');
+			}
+		}
 
 
 		// configure and execute internal API call
 		$urlPrefix = rtrim($Endpoint->InternalEndpoint, '/');
 		$path = '/' . implode('/', static::getPath());
-		
+
 		HttpProxy::relayRequest(array(
 			'autoAppend' => false
 			,'url' => $urlPrefix . $path
 			,'interface' => static::$sourceInterface
 			,'afterResponse' => function($responseBody, $options, $ch) use ($Endpoint, $Key, $path) {
 				$curlInfo = curl_getinfo($ch);
-				
+
 				// log request to database
 				LoggedRequest::create(array(
 					'Endpoint' => $Endpoint
@@ -103,23 +122,23 @@ class ApiRequestHandler extends RequestHandler
 			}
 		));
 	}
-	
+
 	static public function throwKeyError(Endpoint $Endpoint, $error)
 	{
 		header('HTTP/1.0 401 Unauthorized');
 		header('WWW-Authenticate: GateKeeper-Key endpoint="'.$Endpoint->Handle.'"');
 		JSON::error($error);
 	}
-	
+
 	static public function throwRateError($retryAfter = null, $error = 'Rate limit exceeded')
 	{
 		header('HTTP/1.1 429 Too Many Requests');
-		
+
 		if ($retryAfter) {
 			header("Retry-After: $retryAfter");
 			$error .= ", retry after $retryAfter seconds";
 		}
-		
+
 		JSON::error($error);
 	}
 }
--- php-classes/Ban.class.php
+++ php-classes/Ban.class.php
@@ -2,6 +2,8 @@
 
 class Ban extends ActiveRecord
 {
+    static public $tableCachePeriod = 300;
+
 	// ActiveRecord configuration
 	static public $tableName = 'bans';
 	static public $singularNoun = 'ban';
@@ -26,14 +28,14 @@ class Ban extends ActiveRecord
 			,'notnull' => false
 		)
 	);
-	
+
 	static public $relationships = array(
 		'Key' => array(
 			'type' => 'one-one'
 			,'class' => 'Key'
 		)
 	);
-	
+
 	static public $sorters = array(
 		'created' => array(__CLASS__, 'sortCreated')
 		,'expiration' => array(__CLASS__, 'sortExpiration')
@@ -42,19 +44,60 @@ class Ban extends ActiveRecord
 	public function validate($deep = true)
 	{
 		parent::validate($deep);
-		
+
 		if (!$this->KeyID == !$this->IP) {
 			$this->_validator->addError('Ban', 'Ban must specifiy either a API key or an IP address');
 		}
-		
+
 		return $this->finishValidation();
 	}
-	
-	static public function sortExpiration($dir, $name) {
+
+	public function save($deep = true)
+	{
+		parent::save($deep);
+
+		if ($this->isUpdated || $this->isNew) {
+			Cache::delete('bans');
+		}
+	}
+
+	public function destroy()
+	{
+		$success = parent::destroy();
+		Cache::delete('bans');
+	}
+
+	static public function sortExpiration($dir, $name)
+	{
 		return "ExpirationDate $dir";
 	}
-	
-	static public function sortCreated($dir, $name) {
+
+	static public function sortCreated($dir, $name)
+	{
 		return "ID $dir";
+	}
+
+	static public function getActiveBansTable()
+	{
+		if($bans = Cache::fetch('bans')) {
+			return $bans;
+		}
+
+		$bans = array(
+			'ips' => array()
+			,'keys' => array()
+		);
+
+		foreach (Ban::getAllByWhere('ExpirationDate IS NULL OR ExpirationDate > CURRENT_TIMESTAMP') AS $Ban) {
+			if ($Ban->IP) {
+				$bans['ips'][] = long2ip($Ban->IP);
+			} elseif($Ban->KeyID) {
+				$bans['keys'][] = $Ban->KeyID;
+			}
+		}
+
+		Cache::store('bans', $bans, static::$tableCachePeriod);
+
+		return $bans;
 	}
 }
