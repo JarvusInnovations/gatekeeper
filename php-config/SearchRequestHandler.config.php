279a50c90b6dedfa402f8d9695bfe90c23ad0ddb
Chris Alfano <chris@jarv.us>
2014-02-20 16:12:26 -0500

    updated search config

--- php-config/SearchRequestHandler.config.php
+++ php-config/SearchRequestHandler.config.php
@@ -1,20 +1,68 @@
 <?php
 
-SearchRequestHandler::$searchClasses = array(
-	'Endpoint' => array(
+if ($GLOBALS['Session']->hasAccountLevel('User')) {
+    SearchRequestHandler::$searchClasses['User'] = array(
 		'fields' => array(
-			'Title'
-			,array(
-				'field' => 'Handle'
+			array(
+				'field' => 'FirstName'
 				,'method' => 'like'
+			), array(
+				'field' => 'LastName'
+				,'method' => 'like'
+			), array(
+				'field' => 'Username'
+				,'method' => 'like'
+			), array(
+				'field' => 'FullName'
+				,'method' => 'sql'
+				,'sql' => 'CONCAT(FirstName," ",LastName) = "%s"'
 			)
 		)
-		,'conditions' => array('Class' => 'Endpoint')
-	)
-	,'Key' => array(
-		'fields' => array(
-			'OwnerName'
+		,'conditions' => array('AccountLevel != "Deleted"')
+	);
+}
+
+SearchRequestHandler::$searchClasses['Endpoint'] = array(
+	'fields' => array(
+		'Title'
+    	,array(
+			'field' => 'Handle'
+			,'method' => 'like'
 		)
-		,'conditions' => array('Class' => 'Key')
+        ,'AdminName'
+		,array(
+			'field' => 'AdminEmail'
+			,'method' => 'like'
+		)
+	)
+);
+
+SearchRequestHandler::$searchClasses['Key'] = array(
+	'fields' => array(
+		'OwnerName'
+        ,'ContactName'
+        ,array(
+			'field' => 'ContactEmail'
+			,'method' => 'like'
+		)
+        ,array(
+			'field' => 'Key'
+			,'method' => 'like'
+		)
+	)
+);
+
+SearchRequestHandler::$searchClasses['Ban'] = array(
+	'fields' => array(
+		'Notes'
+        ,array(
+    		'field' => 'Notes'
+			,'method' => 'like'
+		)
+        ,array(
+			'field' => 'IP'
+            ,'method' => 'sql'
+			,'sql' => 'INET_NTOA(IP) LIKE "%%%s%%"'
+		)
 	)
 );
