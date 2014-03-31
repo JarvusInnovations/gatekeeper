86faa3fa538f685400585ae7202ff5d83bc323a7
Chris Alfano <chris@jarv.us>
2014-03-31 19:04:29 -0400

    added params for cachable test api

--- site-root/test-api/cachable.php
+++ site-root/test-api/cachable.php
@@ -1,6 +1,8 @@
 <?php
 
-header('Expires: ' . gmdate('D, d M Y H:i:s \G\M\T', time() + 30));
+$cacheSecs = !empty($_GET['secs']) && ctype_digit($_GET['secs']) ? $_GET['secs'] : 30;
+
+header('Expires: ' . gmdate('D, d M Y H:i:s \G\M\T', time() + $cacheSecs));
 
 JSON::respond(array(
     'success' => true,
