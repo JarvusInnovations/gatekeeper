f3e69ad5cf20e1ebdf6e097f0c66a3c1b48c7bf4
Chris Alfano <chris@jarv.us>
2014-03-31 19:03:28 -0400

    tweaked site config to skip sessions on some paths

--- php-config/Site.config.php
+++ php-config/Site.config.php
@@ -2,7 +2,10 @@
 
 Site::$debug = true; // set to true for extended query logging
 Site::$production = true; // set to true for heavy file caching
+#Site::$autoPull = false;
 
 Site::$permittedOrigins = '*';
 
 Site::$skipSessionPaths[] = 'api.php';
+Site::$skipSessionPaths[] = 'test-api/cachable.php';
+Site::$skipSessionPaths[] = 'test-api/status.php';
