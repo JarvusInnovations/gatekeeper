dcabe88e29fc39d040ab60a8447f05404e6383e8
Chris Alfano <chris@jarv.us>
2014-05-22 22:37:58 -0400

    restrict input values in test-api/status endpoint

--- site-root/test-api/status.php
+++ site-root/test-api/status.php
@@ -1,7 +1,7 @@
 <?php
 
-$code = !empty($_REQUEST['statusCode']) ? $_REQUEST['statusCode'] : 200;
-$message = !empty($_REQUEST['statusMessage']) ? $_REQUEST['statusMessage'] : 'OK';
+$code = !empty($_REQUEST['statusCode']) && is_numeric($_REQUEST['statusCode']) ? $_REQUEST['statusCode'] : 200;
+$message = !empty($_REQUEST['statusMessage']) && preg_match('/^[a-zA-Z0-9 \\-]+$/', $_REQUEST['statusMessage']) ? $_REQUEST['statusMessage'] : 'OK';
 
 header("HTTP/1.1 $code $message");
 
