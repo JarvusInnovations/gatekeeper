05f44403f5ef86d5a00f428b2c2184a152a971a2
Chris Alfano <chris@jarv.us>
2014-12-24 20:59:02 -0500

    added email preview endpoint for alerts

--- php-classes/Gatekeeper/Alerts/AlertsRequestHandler.php
+++ php-classes/Gatekeeper/Alerts/AlertsRequestHandler.php
@@ -54,6 +54,8 @@ class AlertsRequestHandler extends \RecordsRequestHandler
                 return static::handleAcknowledgeRequest($Alert);
             case 'dismiss':
                 return static::handleDismissRequest($Alert);
+            case 'email-preview':
+                return static::handleEmailPreviewRequest($Alert);
             default:
                 return parent::handleRecordRequest($Alert, $action);
         }
@@ -82,7 +84,7 @@ class AlertsRequestHandler extends \RecordsRequestHandler
     public static function handleDismissRequest(AbstractAlert $Alert)
     {
         $GLOBALS['Session']->requireAccountLevel('Staff');
-        
+
         if ($_SERVER['REQUEST_METHOD'] != 'POST') {
             return static::respond('confirm', [
                 'question' => 'Are you sure you want to dismiss alert <strong>'.htmlspecialchars($Alert->getTitle()).'</strong>?',
@@ -98,5 +100,24 @@ class AlertsRequestHandler extends \RecordsRequestHandler
             'success' => true,
             'data' => $Alert
         ]);
+    }
+
+    public static function handleEmailPreviewRequest(AbstractAlert $Alert)
+    {
+        $GLOBALS['Session']->requireAccountLevel('Staff');
+
+        $email = \Emergence\Mailer\AbstractMailer::renderTemplate('alerts/notifications/' . $Alert::$notificationTemplate, ['Alert' => $Alert]);
+
+        \Debug::dumpVar($Alert->getData(), false, 'Alert');
+
+        print '<h2>Email</h2>';
+        print '<pre>';
+        print 'From: ' . ($email['from'] ?: \Emergence\Mailer\AbstractMailer::getDefaultFrom()) . PHP_EOL;
+        print 'Subject: ' . $email['subject'] . PHP_EOL . PHP_EOL;
+        print htmlspecialchars($email['body']);
+        print '</pre>';
+
+        print '<h2>Preview</h2>';
+        print '<div style="border: 1px dashed #ccc; width: 600px; font-family: arial,sans-serif; font-size: 80%; padding: 15px">' . $email['body'] . '</div>';
     }
 }
