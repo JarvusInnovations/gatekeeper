2fe20feb0d38dc8c1e3225d0049564bc1b750286
Chris Alfano <chris@jarv.us>
2016-01-13 21:45:47 -0500

    Convert Site config override to partial configs

--- php-config/Site.config.d/gatekeeper.php
+++ php-config/Site.config.d/gatekeeper.php
@@ -1,11 +1,5 @@
 <?php
 
-Site::$debug = true; // set to true for extended query logging
-Site::$production = true; // set to true for heavy file caching
-#Site::$autoPull = false;
-
-Site::$permittedOrigins = '*';
-
 Site::$skipSessionPaths[] = 'api.php';
 Site::$skipSessionPaths[] = 'test-api/cachable.php';
 Site::$skipSessionPaths[] = 'test-api/status.php';
