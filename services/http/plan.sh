14ed933385bfb7abebbbce0799b26904030dffa7
Chris Alfano <chris@jarv.us>
2018-04-30 00:15:28 -0400

    Add nginx service

---
+++ services/http/plan.sh
@@ -0,0 +1,23 @@
+pkg_name=gatekeeper-http
+pkg_origin=jarvus
+pkg_version="0.1.0"
+pkg_maintainer="Chris Alfano <chris@jarv.us>"
+pkg_license=("AGPL-3.0")
+pkg_deps=(
+  emergence/nginx
+)
+
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  return 0
+}
+
+do_build_config() {
+  do_default_build_config || return $?
+
+  cp -vnr "$(pkg_path_for emergence/nginx)/config"/*.include "${pkg_prefix}/config/"
+}
