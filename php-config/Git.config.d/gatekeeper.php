90c6d66853ec7b6514e126637e9e614c9c5eb82a
Chris Alfano <chris@jarv.us>
2018-06-21 04:58:50 +0000

    Split up Git.config.php

--- php-config/Git.config.d/gatekeeper.php
+++ php-config/Git.config.d/gatekeeper.php
@@ -21,12 +21,3 @@ Git::$repositories['gatekeeper'] = [
         ]
     ]
 ];
-
-Git::$repositories['jarvus-highlighter'] = [
-    'remote' => "https://github.com/JarvusInnovations/jarvus-highlighter.git",
-    'originBranch' => 'master',
-    'workingBranch' => 'master',
-    'trees' => [
-        "sencha-workspace/packages/jarvus-highlighter" => '.'
-    ]
-];
---
+++ php-config/Git.config.d/jarvus-highlighter.php
@@ -0,0 +1,10 @@
+<?php
+
+Git::$repositories['jarvus-highlighter'] = [
+    'remote' => "https://github.com/JarvusInnovations/jarvus-highlighter.git",
+    'originBranch' => 'master',
+    'workingBranch' => 'master',
+    'trees' => [
+        "sencha-workspace/packages/jarvus-highlighter" => '.'
+    ]
+];
