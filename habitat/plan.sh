9225c6f14154f069bec7166df2d1eccac81e321a
Nafis Bey <nafis@jarv.us>
2020-08-14 03:33:43 +0000

    feat: implement docker builds
    - create gatekeeper-composite habitat plan

---
+++ habitat/plan.sh
@@ -0,0 +1,8 @@
+pkg_name=gatekeeper
+pkg_origin=jarvus
+pkg_maintainer="Jarvus Innovations <hello@jarv.us>"
+pkg_scaffolding=emergence/scaffolding-site
+
+pkg_version() {
+  scaffolding_detect_pkg_version
+}
